073b8b86551cccfc7fbb30e13b118170bff5c8d6
Dani-3108 <floresd477@gmail.com>
2026-03-05 21:46:17 -0600

    Programming Exercise 3: Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        String keepPlaying = "yes";
+        while (keepPlaying == "yes")
+        {
+            //random number
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            string userInput = "";
+            int guess = 0;
+            int guessCount = 0;
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                userInput = Console.ReadLine();
+                guess = int.Parse(userInput);
+                guessCount = guessCount + 1;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+            Console.WriteLine($"It took you {guessCount} guesses to guess the secret number.");
+
+            Console.Write("Do you want to keep playing? ");
+            keepPlaying = Console.ReadLine();
+        }
     }
 }
