5ee716631ea49ba0714e3013759ec536d9bcbccc
Dani-3108 <floresd477@gmail.com>
2026-03-05 21:44:14 -0600

    Programming Exercise 1: Input and Output

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //User Input
+        Console.Write("What is your first name? ");
+        String firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        String lastName = Console.ReadLine();
+
+        //print statements
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
