0a585acd144111a832c61ce6e587e4ed61521e69
Dani-3108 <floresd477@gmail.com>
2026-03-09 22:40:36 -0600

    Learning Activity: Abstraction Completed

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,14 @@
+public class Job
+{
+    //Attributes
+    public String _company;
+    public String _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    //Behaviors
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}.");
+    }
+}
